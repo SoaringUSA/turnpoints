63af77c47998d3e98d6987bbde5ffd50f79d7dec
Philip Lee <752558+rocketman768@users.noreply.github.com>
2021-04-24 09:30:07 -0700

    Add release script

---
+++ release.sh
@@ -0,0 +1,16 @@
+#!/bin/sh -e
+
+CUPFILES="hllstr.cup"
+DATE=$(date '+%Y%m%d')
+
+TMPDIR=$(mktemp -d)
+DIR=$TMPDIR/SUSATurnpoints
+mkdir $DIR
+for cupfile in $CUPFILES; do
+	BNAME="$(basename $cupfile .cup)_$DATE"
+	cp $cupfile "$DIR/$BNAME.cup"
+	./tokml.py < $cupfile > "$DIR/$BNAME.kml"
+done
+
+tar -czf "SUSATurnpoints_$DATE.tar.gz" -C $TMPDIR .
+rm -rf $TMPDIR
