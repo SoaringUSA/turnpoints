fcaff6e8fd6195136d04291e97762000afd9ceab
Philip Lee <752558+rocketman768@users.noreply.github.com>
2022-04-22 08:14:44 -0700

    Modify all the to*.py scripts to input json format

--- tostx.py
+++ tostx.py
@@ -5,21 +5,6 @@ import re
 import sys
 import xml.etree.ElementTree as ET
 
-def parseLat(latStr):
-	# 3653.583N,12115.800W
-	match = re.match('(\d*)(\d\d[.]\d*)([NS])', latStr)
-	deg = float(match.group(1)) + float(match.group(2)) / 60.0
-	if match.group(3) == 'S':
-		deg = -deg
-	return deg
-
-def parseLon(lonStr):
-	match = re.match('(\d*)(\d\d[.]\d*)([EW])', lonStr)
-	deg = float(match.group(1)) + float(match.group(2)) / 60.0
-	if match.group(3) == 'W':
-		deg = -deg
-	return deg
-
 def dms(decimalDeg):
 	sign = False if decimalDeg < 0 else True
 	decimalDeg = abs(decimalDeg)
@@ -35,18 +20,8 @@ def dms(decimalDeg):
 	# West is positive degrees?
 	return [d,m,s]
 
-def parseLength(elevStr):
-	if not elevStr:
-		return ''
-	match = re.match('(\d*[.]?\d*)(m|ft)', elevStr)
-	value = float(match.group(1))
-	unit = match.group(2)
-	if unit == 'm':
-		return int(value * 3.28084)
-	elif unit == 'ft':
-		return int(value)
-	else:
-		raise ValueError('Unknown length unit {0}'.format(unit))
+def parseLength(meters):
+	return int(meters * 3.28084)
 
 def parseRwdir(rwdir):
 	if not rwdir:
@@ -63,27 +38,34 @@ def parseRwdir(rwdir):
 	return '{0}/{1}'.format(d, dd)
 
 if __name__=='__main__':
-	# name,code,country,lat,lon,elev,style,rwdir,rwlen,freq,desc,userdata,pics
-	cvr = csv.DictReader(sys.stdin, fieldnames=['name','code','country','lat','lon','elev','style','rwdir','rwlen','freq','desc','userdata','pics'])
-	# Skip first row
-	next(cvr)
+	input = json.load(sys.stdin)
 
 	cvw = csv.DictWriter(sys.stdout, fieldnames=['NUM','NAM','COM','LAT_D','LAT_M','LAT_S','LON_D','LON_M','LON_S','COD','ALT','FRE','RW_D','RW_H','RW_L','RW_S','RW_W','RW_G','ICAO','NAM_12','COM_12','CIT','CUP_COD','FAA_NAME','FRE_ATIS','FRE_AWOS','FRE_CTAF','FRE_TOWER','FRE_UNICOM','FUEL','GLIDERS','MAG','STA','STATE_ABBREV','TEL'], delimiter='	', quoting=csv.QUOTE_NONE)
 	cvw.writeheader()
 
 	rowNum = 0
-	for row in cvr:
-		if not row['lat']:
-			continue
+	for (name, tp) in input.items():
 		rowNum += 1
-		name = row['name']
-		latDms = dms(parseLat(row['lat']))
-		lonDms = dms(parseLon(row['lon']))
+		latDms = dms(tp['lat'])
+		lonDms = dms(tp['lon'])
 		
+		freq = ''
+		rwdir = ''
+		rwlen = ''
+		if 'landable' in tp
+			if 'runways' in tp['landable'] and len(tp['landable']['runways']) > 0:
+				rw0 = tp['landable']['runways'][0]
+				if 'dir' in rw0:
+					rwdir = rw0['dir']
+				if 'len' in rw0:
+					rwlen = rw0['len']
+			if 'freq' in tp['landable']:
+				freq = tp['landable']['freq']
+
 		outDict = {
 			'NUM' : rowNum,
-			'NAM' : row['name'],
-			'COM' : row['desc'],
+			'NAM' : name,
+			'COM' : tp['desc'] if 'desc' in tp else '',
 			'LAT_D' : latDms[0],
 			'LAT_M' : latDms[1],
 			'LAT_S' : latDms[2],
@@ -91,11 +73,11 @@ if __name__=='__main__':
 			'LON_M' : lonDms[1],
 			'LON_S' : lonDms[2],
 			'COD' : '',
-			'ALT' : parseLength(row['elev']),
-			'FRE' : row['freq'],
-			'RW_D' : parseRwdir(row['rwdir']),
+			'ALT' : parseLength(tp['elev']),
+			'FRE' : freq,
+			'RW_D' : rwdir,
 			'RW_H' : '',
-			'RW_L' : parseLength(row['rwlen']),
+			'RW_L' : rwlen,
 			'RW_S' : '',
 			'RW_W' : '',
 			'RW_G' : '',
