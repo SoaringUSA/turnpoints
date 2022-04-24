bc82a7e41caa72180705320e12a7c721c1eff4fe
Philip Lee <752558+rocketman768@users.noreply.github.com>
2022-04-24 07:21:08 -0700

    Validate before release

--- release.sh
+++ release.sh
@@ -1,7 +1,15 @@
 #!/bin/sh -e
 
+enable -n echo
+
 INFILES="hllstr.json truckee.json wsc.json"
 DATE=$(date '+%Y%m%d')
+
+for infile in $INFILES; do
+	echo -n "Validating $infile..."
+	./validate.py < $infile
+	echo 'OK'
+done
 
 TMPDIR=$(mktemp -d)
 DIR=$TMPDIR/SUSATurnpoints
@@ -15,8 +23,9 @@ done
 
 # California
 ./combine.py 'California' hllstr.json wsc.json truckee.json > "$DIR/california_$DATE.json"
+./validate.py < "$DIR/california_$DATE.json"
 ./tocup.py < "$DIR/california_$DATE.json" > "$DIR/california_$DATE.cup"
 ./tokml.py < "$DIR/california_$DATE.json" > "$DIR/california_$DATE.kml"
 
 tar -czf "SUSATurnpoints_$DATE.tar.gz" -C $TMPDIR .
-#rm -rf $TMPDIR
+rm -rf $TMPDIR
