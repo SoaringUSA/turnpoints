92653179d1cb684eab8fa50949062bb0423078a4
Philip Lee <752558+rocketman768@users.noreply.github.com>
2022-05-08 22:51:58 -0700

    Add ability to cut turnpoints by state

--- cut.py
+++ cut.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import geocoder
 import json
 import sys
 
@@ -8,7 +9,7 @@ from wgs84 import WGS84
 def help():
     print('Pare a large turnpoint set into a smaller one', sys.stderr)
     print('USAGE: ./cut.py <COMMAND> options...', sys.stderr)
-    print('  COMMAND = box', sys.stderr)
+    print('  COMMAND = box, radius, state', sys.stderr)
 
 def helpBox():
     print('./cut.py box <lat0> <lat1> <lon0> <lon1>', sys.stderr)
@@ -17,6 +18,14 @@ def helpBox():
 def helpRadius():
     print('./cut.py radius <lat> <lon> <radius_km>', sys.stderr)
     print('  Selects all turnpoints within <radius_km> kilometers of the given position. All coordinates are decimal degrees.', sys.stderr)
+
+def helpState():
+    print('./cut.py state <state_name>', sys.stderr)
+    print('  Selects all turnpoints in the given state. Can be very slow due to internet query.', sys.stderr)
+
+def isInState(tp, stateName):
+    query = geocoder.arcgis('{0}, {1}'.format(tp['lat'], tp['lon']), method='reverse')
+    return query.state.lower() == stateName.lower()
 
 def isInBox(tp, lat0, lat1, lon0, lon1):
     return lat0 <= tp['lat'] and tp['lat'] <= lat1 and lon0 <= tp['lon'] and tp['lon'] <= lon1
@@ -54,6 +63,12 @@ if __name__=='__main__':
         lon = float(sys.argv[3])
         distKm = float(sys.argv[4])
         filtFun = lambda x: isInRadius(x, lat, lon, distKm)
+    elif command == 'state':
+        if len(sys.argv) < 3:
+            helpState()
+            sys.exit(1)
+        state = sys.argv[2]
+        filtFun = lambda x: isInState(x, state)
     else:
         help()
         sys.exit(1)
